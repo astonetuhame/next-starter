8069bf10ffd59d591539a897349b2aeb594d322c
Astone Tuhame <astonetuhame@gmail.com>
2021-12-18 23:30:09 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,9 @@
-import Head from "next/head"
+import Meta from "../components/Meta"
+
 const about = () => {
     return (
         <div>
-            <Head>
-                <title>About</title>
-            </Head>
+            <Meta title='About'></Meta>
             <h1>About</h1>
         </div>
     )
