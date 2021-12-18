a446cdc0df979e294612ecc39d1ec10249820e16
Astone Tuhame <astonetuhame@gmail.com>
2021-12-18 23:30:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
+import {server} from '../config'
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
   
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
-      <Head>
-        <title>Developer News</title>
-        <meta name="keywords" content="web, development, programming"/>
-      </Head>
-     <h1>Welcome to Next</h1>
+      <ArticleList articles={articles} />
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  
+  const res = await fetch(`${server}/api/articles`)
+
+  const articles = await res.json()
+
+  return {
+      props: {
+        articles
+      }
+}
+    
+}
+// export const getStaticProps = async () => {
+  
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+//   const articles = await res.json()
+
+//   return {
+//       props: {
+//         articles
+//       }
+// }
+    
+// }
